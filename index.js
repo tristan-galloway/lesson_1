13f35e8d68a63500ec37b5f3380dfce3dba28e2a
Tristan Galloway <tristan.a.galloway@gmail.com>
2025-01-11 20:55:20 -0700

    Added a route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello, Render!');
 });
 
+// New route to return a name
+app.get('/name', (req, res) => {
+  res.send('John Doe');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
